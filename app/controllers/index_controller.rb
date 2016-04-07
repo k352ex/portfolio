7eeefd26914663116ab28db3811c166e0ea2afab
k352ex <k352ex@naver.com>
2016-04-08 01:20:05 +0900

    뷰에서 처리하던 데이터 컨트롤러로 일부 이동 및 follow 아이콘 색상 변경

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,23 @@
 class IndexController < ApplicationController
 	def main
 
+		@name = "박민기"
+
+		@header_intro = [
+			"Hello world!",
+			"I'll become WebDeveloper",
+			"Coding is fun.",
+			"Making the Website :)"
+		]
+
+		@skills = {
+			"FrontEnd" => ["HTML","CSS","Javascript"],
+			"BackEnd"  => ["Ruby On Rails", "Python-Django"],
+			"Language" => ["Python", "Ruby"]
+		}
+
+		@email = "k352ex@naver.com"
+		
 	end
 
 	def fullpage
