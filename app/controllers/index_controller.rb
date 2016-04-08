2538cf21938c7b7bf15c85deb6497575fe657aaf
k352ex <k352ex@naver.com>
2016-04-08 18:51:29 +0900

    구글 웹로그 추적코드 삽입

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,18 +10,23 @@ class IndexController < ApplicationController
 			"Making the Website :)"
 		]
 
+		# Skill
 		@skills = {
 			"FrontEnd" => ["HTML","CSS","Javascript"],
 			"BackEnd"  => ["Ruby On Rails", "Python-Django"],
 			"Language" => ["Python", "Ruby"]
 		}
 
+		# Project
+		@project = ["원광대학교 컴퓨터공학과 커뮤니티 사이트"]
+
+		# URL
 		@email = "k352ex@naver.com"
 		@github_url = "https://github.com/k352ex"
 		@facebook_url = "https://www.facebook.com/k352ex"
-
-
+		@linkedin_url = "https://kr.linkedin.com/in/minki-pak-74b791116"
 		@project_url = ["https://github.com/k352ex/WKU_Computer"]
+
 	end
 
 	def fullpage
